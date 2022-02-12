804c078210349a4a0857e45b2cf08d872c8c2fa3
12moi <moi.shadrack@student.moringaschool.com>
2022-02-12 20:40:02 +0300

    add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {path:"search-repo", component:RepositoryComponent},
+  {path: "", redirectTo:"/user-profile", pathMatch:"full"},
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
