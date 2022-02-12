20b9a7faf6dd99a0fb847fb39ed0eb871a592be0
12moi <moi.shadrack@student.moringaschool.com>
2022-02-12 09:57:14 +0300

    style repository component

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from '../repos';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-repository',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+  repo!: Repos;
+  constructor( public repoService: UserServiceService ) { }
 
-  constructor() { }
+  repoSearch(searchName: any){
+    this.repoService['getReopos'](searchName).then(
+      (results: any)=>{
+        this.repo =this.repoService['allRepos']
+        console.log(this.repo);
+      },
+      (error: any)=>{
+        console.log(error);
+      }
+    );
+  }
+
+ 
 
   ngOnInit(): void {
   }
