5e6c481b0877f7fb537e651e4b499ce9c3bd0636
12moi <moi.shadrack@student.moringaschool.com>
2022-02-13 09:32:53 +0300

    style the user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Repos } from '../repos';
+ import { User } from '../user';
+import { UserServiceService } from '../user-service.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+  repo!: Repos;
+  repoService: any;
+  constructor(public myService: UserServiceService, private Repo: UserServiceService) {
+  }
+
+  searchs(searchName: any) {
+    this.myService['searchUSer'](searchName).then(
+      (success: any)=>{
+        this.user = this.myService['foundUser'];
+      },
+      (error: any)=>{
+        console.log(error)
+      }
+    );
+      this.repoService.getReopos(searchName).then(
+        (results: any)=>{
+          this.repo =this.repoService.allRepos
+          console.log(this.repo);
+        },
+        (error: any)=>{
+          console.log(error);
+        }
+      );
+  }
 
   ngOnInit(): void {
   }
